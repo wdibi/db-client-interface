fbdd8ad107a251bce189fd34013fafbdf404f24b
Marco Berardini <marcob@marcos-mac-mini.lan>
2020-12-16 22:59:34 -0800

    remove mile from cards

--- src/components/construction/ConstructionCard.jsx
+++ src/components/construction/ConstructionCard.jsx
@@ -1,18 +1,30 @@
-import React from "react"
-import { Card } from "react-bootstrap"
+/** @format */
+
+import React from 'react'
+import { Card } from 'react-bootstrap'
 
 export default function ConstructionCard({ handleOnClick, construction }) {
-  let {permit_type, id, location: {coordinates}} = construction
+  let {
+    permit_type,
+    id,
+    location: { coordinates },
+  } = construction
   return (
-    <Card key={construction._id} style={{ margin: "10px 0px" }} className="text-center">
-      <Card.Body className="card-body" onClick={() => handleOnClick(construction)}>
+    <Card
+      key={construction._id}
+      style={{ margin: '10px 0px' }}
+      className='text-center'
+    >
+      <Card.Body
+        className='card-body'
+        onClick={() => handleOnClick(construction)}
+      >
         <Card.Title>{permit_type}</Card.Title>
         <Card.Text></Card.Text>
         <Card.Text>
-        long: {coordinates[0]} - lat:  {coordinates[1]} miles
+          long: {coordinates[0]} - lat: {coordinates[1]}
         </Card.Text>
       </Card.Body>
-
     </Card>
   )
 }
--- src/components/streetlight/StreetLightCard.jsx
+++ src/components/streetlight/StreetLightCard.jsx
@@ -1,22 +1,42 @@
-import React , {useContext} from "react"
-import { Card, Button } from "react-bootstrap"
-import {Context as StoreContext} from '../../context/StoreContext'
+/** @format */
 
+import React, { useContext } from 'react'
+import { Card, Button } from 'react-bootstrap'
+import { Context as StoreContext } from '../../context/StoreContext'
 
 export default function CrimeCard({ handleOnClick, streetLight }) {
-   let { id, location: {coordinates}} = streetLight
-   const {deleteStreetLight, editStreetLight} = useContext(StoreContext)
+  let {
+    id,
+    location: { coordinates },
+  } = streetLight
+  const { deleteStreetLight, editStreetLight } = useContext(StoreContext)
 
   return (
-    <Card  key={id} style={{ margin: "10px 0px" }} className="text-center">
-      <Card.Body className="card-body" >
-        <Card.Title ><span 
-             onClick={handleOnClick} style={{position: "absolute" ,top: 10, left: 10, cursor: "pointer"}}>✏️</span>{id}</Card.Title>
+    <Card key={id} style={{ margin: '10px 0px' }} className='text-center'>
+      <Card.Body className='card-body'>
+        <Card.Title>
+          <span
+            onClick={handleOnClick}
+            style={{
+              position: 'absolute',
+              top: 10,
+              left: 10,
+              cursor: 'pointer',
+            }}
+          >
+            ✏️
+          </span>
+          {id}
+        </Card.Title>
         <Card.Text>
-        long: {coordinates[0]} - lat:  {coordinates[1]} miles        </Card.Text>
- 
+          long: {coordinates[0]} - lat: {coordinates[1]}{' '}
+        </Card.Text>
       </Card.Body>
-      <Card.Footer><Button onClick={() => deleteStreetLight(id)} variant="outline-danger">Delete</Button></Card.Footer>
+      <Card.Footer>
+        <Button onClick={() => deleteStreetLight(id)} variant='outline-danger'>
+          Delete
+        </Button>
+      </Card.Footer>
     </Card>
   )
 }
